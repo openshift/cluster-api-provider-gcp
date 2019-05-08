02419e70ee6ac8808050fa72529a2fc72969c346
Enxebre <alberto.garcial@hotmail.com>
2019-05-08 10:06:08 +0200

    Implement create method
    This follows a similar pattern to azure and aws upstream. Separate responsibilities for each request into actuator, machine_scope, reconciler and computeservice. Machine_scope contains the context and client boundaries for each request. Reconciler reconciles with external state by leveraging the machine scope and the computeservice

---
+++ pkg/cloud/gcp/actuators/machine/actuator.go
@@ -0,0 +1,62 @@
+package machine
+
+import (
+	"context"
+	"fmt"
+
+	clusterv1 "github.com/openshift/cluster-api/pkg/apis/cluster/v1alpha1"
+	machinev1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
+	mapiclient "github.com/openshift/cluster-api/pkg/client/clientset_generated/clientset/typed/machine/v1beta1"
+	"k8s.io/klog"
+	controllerclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Actuator is responsible for performing machine reconciliation.
+type Actuator struct {
+	machineClient mapiclient.MachineV1beta1Interface
+	coreClient    controllerclient.Client
+}
+
+// ActuatorParams holds parameter information for Actuator.
+type ActuatorParams struct {
+	MachineClient mapiclient.MachineV1beta1Interface
+	CoreClient    controllerclient.Client
+}
+
+// NewActuator returns an actuator.
+func NewActuator(params ActuatorParams) *Actuator {
+	return &Actuator{
+		machineClient: params.MachineClient,
+		coreClient:    params.CoreClient,
+	}
+}
+
+// Create creates a machine and is invoked by the machine controller.
+func (a *Actuator) Create(ctx context.Context, cluster *clusterv1.Cluster, machine *machinev1.Machine) error {
+	klog.Infof("Creating machine %v", machine.Name)
+	scope, err := newMachineScope(machineScopeParams{
+		machineClient: a.machineClient,
+		coreClient:    a.coreClient,
+		machine:       machine,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create scope for machine %q: %v", machine.Name, err)
+	}
+	defer scope.Close()
+	return newReconciler(scope).create()
+}
+
+func (a *Actuator) Exists(ctx context.Context, cluster *clusterv1.Cluster, machine *machinev1.Machine) (bool, error) {
+	// TODO(alberto): implement this
+	return false, nil
+}
+
+func (a *Actuator) Update(ctx context.Context, cluster *clusterv1.Cluster, machine *machinev1.Machine) error {
+	// TODO(alberto): implement this
+	return nil
+}
+
+func (a *Actuator) Delete(ctx context.Context, cluster *clusterv1.Cluster, machine *machinev1.Machine) error {
+	// TODO(alberto): implement this
+	return nil
+}
---
+++ pkg/cloud/gcp/actuators/services/compute/computeservice.go
@@ -0,0 +1,39 @@
+package computeservice
+
+import (
+	"net/http"
+
+	"google.golang.org/api/compute/v1"
+)
+
+// GCPComputeService is a pass through wrapper for google.golang.org/api/compute/v1/compute
+// to enable tests to mock this struct and control behavior.
+type GCPComputeService interface {
+	InstancesInsert(project string, zone string, instance *compute.Instance) (*compute.Operation, error)
+	ZoneOperationsGet(project string, zone string, operation string) (*compute.Operation, error)
+}
+
+type computeService struct {
+	service *compute.Service
+}
+
+// NewComputeService return a new computeService
+func NewComputeService(oauthClient *http.Client) (*computeService, error) {
+	service, err := compute.New(oauthClient)
+	if err != nil {
+		return nil, err
+	}
+	return &computeService{
+		service: service,
+	}, nil
+}
+
+// InstancesInsert is a pass through wrapper for compute.Service.Instances.Insert(...)
+func (c *computeService) InstancesInsert(project string, zone string, instance *compute.Instance) (*compute.Operation, error) {
+	return c.service.Instances.Insert(project, zone, instance).Do()
+}
+
+// ZoneOperationsGet is a pass through wrapper for compute.Service.ZoneOperations.Get(...)
+func (c *computeService) ZoneOperationsGet(project string, zone string, operation string) (*compute.Operation, error) {
+	return c.service.ZoneOperations.Get(project, zone, operation).Do()
+}
