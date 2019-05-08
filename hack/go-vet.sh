30c3915db65b2651ab6565720bbbe834e8dc8e72
Enxebre <alberto.garcial@hotmail.com>
2019-05-08 12:10:14 +0200

    Add basic make file: unit, fmt, vet, build

---
+++ hack/go-vet.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ "$IS_CONTAINER" != "" ]; then
+  go vet "${@}"
+else
+  docker run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/openshift/cluster-api-provider-gcp:z" \
+    --workdir /go/src/github.com/openshift/cluster-api-provider-gcp \
+    openshift/origin-release:golang-1.10 \
+    ./hack/go-vet.sh "${@}"
+fi;
