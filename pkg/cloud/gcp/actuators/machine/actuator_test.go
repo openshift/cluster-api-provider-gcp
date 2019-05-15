f124431c730104794ccf3463a95e99c38719a9fe
Andrew McDermott <amcdermo@redhat.com>
2019-05-15 19:19:57 +0100

    Stub out test

---
+++ pkg/cloud/gcp/actuators/machine/actuator_test.go
@@ -0,0 +1,19 @@
+package machine_test
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+	// TODO(frobware)
+}
+
+func TestUpdate(t *testing.T) {
+	// TODO(frobware)
+}
+
+func TestDelete(t *testing.T) {
+	// TODO(frobware)
+}
+
+func TestExists(t *testing.T) {
+	// TODO(frobware)
+}
