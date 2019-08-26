7d2d26be548d227b127bdc1e16fa425ce4448192
Jan Chaloupka <jchaloup@redhat.com>
2019-08-26 14:49:51 +0200

    wrap machine config/status coding/decoding under a codec

--- pkg/apis/gcpprovider/v1beta1/register.go
+++ pkg/apis/gcpprovider/v1beta1/register.go
@@ -7,11 +7,14 @@
 package v1beta1
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 
+	machinev1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/klog"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 	"sigs.k8s.io/yaml"
@@ -24,6 +27,85 @@ var (
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
 )
+
+// GCPProviderConfigCodec is a runtime codec for the provider configuration
+// +k8s:deepcopy-gen=false
+type GCPProviderConfigCodec struct {
+	encoder runtime.Encoder
+	decoder runtime.Decoder
+}
+
+// NewScheme creates a new Scheme
+func NewScheme() (*runtime.Scheme, error) {
+	return SchemeBuilder.Build()
+}
+
+// NewCodec creates a serializer/deserializer for the provider configuration
+func NewCodec() (*GCPProviderConfigCodec, error) {
+	scheme, err := NewScheme()
+	if err != nil {
+		return nil, err
+	}
+	codecFactory := serializer.NewCodecFactory(scheme)
+	encoder, err := newEncoder(&codecFactory)
+	if err != nil {
+		return nil, err
+	}
+	codec := GCPProviderConfigCodec{
+		encoder: encoder,
+		decoder: codecFactory.UniversalDecoder(SchemeGroupVersion),
+	}
+	return &codec, nil
+}
+
+// DecodeProviderSpec deserialises an object from the provider config
+func (codec *GCPProviderConfigCodec) DecodeProviderSpec(providerSpec *machinev1.ProviderSpec, out runtime.Object) error {
+	if providerSpec.Value != nil {
+		if _, _, err := codec.decoder.Decode(providerSpec.Value.Raw, nil, out); err != nil {
+			return fmt.Errorf("decoding failure: %v", err)
+		}
+	}
+	return nil
+}
+
+// EncodeProviderSpec serialises an object to the provider config
+func (codec *GCPProviderConfigCodec) EncodeProviderSpec(in runtime.Object) (*machinev1.ProviderSpec, error) {
+	var buf bytes.Buffer
+	if err := codec.encoder.Encode(in, &buf); err != nil {
+		return nil, fmt.Errorf("encoding failed: %v", err)
+	}
+	return &machinev1.ProviderSpec{
+		Value: &runtime.RawExtension{Raw: buf.Bytes()},
+	}, nil
+}
+
+// EncodeProviderStatus serialises the provider status
+func (codec *GCPProviderConfigCodec) EncodeProviderStatus(in runtime.Object) (*runtime.RawExtension, error) {
+	var buf bytes.Buffer
+	if err := codec.encoder.Encode(in, &buf); err != nil {
+		return nil, fmt.Errorf("encoding failed: %v", err)
+	}
+	return &runtime.RawExtension{Raw: buf.Bytes()}, nil
+}
+
+// DecodeProviderStatus deserialises the provider status
+func (codec *GCPProviderConfigCodec) DecodeProviderStatus(providerStatus *runtime.RawExtension, out runtime.Object) error {
+	if providerStatus != nil {
+		if _, _, err := codec.decoder.Decode(providerStatus.Raw, nil, out); err != nil {
+			return fmt.Errorf("decoding failure: %v", err)
+		}
+	}
+	return nil
+}
+
+func newEncoder(codecFactory *serializer.CodecFactory) (runtime.Encoder, error) {
+	serializerInfos := codecFactory.SupportedMediaTypes()
+	if len(serializerInfos) == 0 {
+		return nil, fmt.Errorf("unable to find any serlializers")
+	}
+	encoder := codecFactory.EncoderForVersion(serializerInfos[0].Serializer, SchemeGroupVersion)
+	return encoder, nil
+}
 
 // RawExtensionFromProviderSpec marshals the machine provider spec.
 func RawExtensionFromProviderSpec(spec *GCPMachineProviderSpec) (*runtime.RawExtension, error) {
