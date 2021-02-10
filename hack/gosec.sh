6f8480f441e1fdb4e3796e672466ef1221d88f77
SamuelStuchly <sssstuchly@gmail.com>
2021-02-10 05:39:45 +0100

    Update make targets to run with podman and docker or without container

--- hack/gosec.sh
+++ hack/gosec.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-if [ "$IS_CONTAINER" != "" ]; then
-  go get github.com/securego/gosec/cmd/gosec
-  gosec -severity medium -confidence medium -exclude G304 -quiet "${@}"
-else
-  docker run --rm \
-    --env GO111MODULE="$GO111MODULE" \
-    --env GOFLAGS="$GOFLAGS" \
-    --env GOPROXY="$GOPROXY" \
-    --env IS_CONTAINER=TRUE \
-    --volume "${PWD}:/go/src/github.com/openshift/cluster-api-provider-gcp:z" \
-    --workdir /go/src/github.com/openshift/cluster-api-provider-gcp \
-    openshift/origin-release:golang-1.15 \
-    ./hack/gosec.sh "${@}"
-fi;
+cd /tmp
+go get github.com/securego/gosec/cmd/gosec
+gosec -severity medium -confidence medium -exclude G304 -quiet "${@}"
+cd -
