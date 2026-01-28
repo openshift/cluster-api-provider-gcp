e705de742130accf488ee4552237e009198b71e7
Damiano Donati <damiano.donati@gmail.com>
2026-01-28 14:57:25 +0100

    UPSTREAM: <drop>: Update manifests generator

--- openshift/tools/vendor/github.com/openshift/cluster-capi-operator/manifests-gen/customizations.go
+++ openshift/tools/vendor/github.com/openshift/cluster-capi-operator/manifests-gen/customizations.go
@@ -18,12 +18,6 @@ import (
 
 type resourceKey string
 
-const (
-	crdKey                                                            resourceKey = "crds"
-	otherKey                                                          resourceKey = "other"
-	managedByAnnotationValueClusterCAPIOperatorInfraClusterController             = "cluster-capi-operator-infracluster-controller"
-)
-
 var (
 	openshiftAnnotations = map[string]string{
 		"exclude.release.openshift.io/internal-openshift-hosted":      "true",
@@ -45,10 +39,8 @@ var (
 	featureSetAnnotationKey   = "release.openshift.io/feature-set"
 )
 
-func processObjects(objs []unstructured.Unstructured, providerName string) map[resourceKey][]unstructured.Unstructured {
-	resourceMap := map[resourceKey][]unstructured.Unstructured{}
+func processObjects(objs []unstructured.Unstructured, providerName string) []unstructured.Unstructured {
 	providerConfigMapObjs := []unstructured.Unstructured{}
-	crdObjs := []unstructured.Unstructured{}
 
 	objs = addInfraClusterProtectionPolicy(objs, providerName)
 
@@ -62,34 +54,16 @@ func processObjects(objs []unstructured.Unstructured, providerName string) map[r
 			setNoUpgradeAnnotations(obj)
 			providerConfigMapObjs = append(providerConfigMapObjs, obj)
 		case "MutatingWebhookConfiguration":
-			// Explicitly remove defaulting webhooks for the cluster-api provider.
-			// We don't need CAPI to set any default to the cluster object because
-			// we have a custom controller for reconciling it.
-			// For more information: https://issues.redhat.com/browse/OCPCLOUD-1506
-			removeClusterDefaultingWebhooks(&obj)
 			replaceCertManagerAnnotations(&obj)
 			providerConfigMapObjs = append(providerConfigMapObjs, obj)
 		case "ValidatingWebhookConfiguration":
-			removeClusterValidatingWebhooks(&obj)
 			replaceCertManagerAnnotations(&obj)
 			providerConfigMapObjs = append(providerConfigMapObjs, obj)
 		case "CustomResourceDefinition":
 			replaceCertManagerAnnotations(&obj)
-			removeConversionWebhook(&obj)
 			setOpenShiftAnnotations(obj, true)
-			// Apply NoUpgrade annotations unless IPAM CRDs,
-			// as those are in General Availability.
-			if !isCRDGroup(&obj, "ipam.cluster.x-k8s.io") {
-				setNoUpgradeAnnotations(obj)
-			}
-			// Store Core CAPI CRDs in their own manifest to get them applied by CVO directly.
-			// We want these to be installed independently from whether the cluster-capi-operator is enabled,
-			// as other Openshift components rely on them.
-			if providerName == coreCAPIProvider {
-				crdObjs = append(crdObjs, obj)
-			} else {
-				providerConfigMapObjs = append(providerConfigMapObjs, obj)
-			}
+			setNoUpgradeAnnotations(obj)
+			providerConfigMapObjs = append(providerConfigMapObjs, obj)
 		case "Service":
 			replaceCertMangerServiceSecret(&obj, serviceSecretNames)
 			setOpenShiftAnnotations(obj, true)
@@ -112,10 +86,7 @@ func processObjects(objs []unstructured.Unstructured, providerName string) map[r
 		}
 	}
 
-	resourceMap[crdKey] = crdObjs
-	resourceMap[otherKey] = providerConfigMapObjs
-
-	return resourceMap
+	return providerConfigMapObjs
 }
 
 func setOpenShiftAnnotations(obj unstructured.Unstructured, merge bool) {
@@ -276,17 +247,6 @@ func replaceCertMangerServiceSecret(obj *unstructured.Unstructured, serviceSecre
 	}
 }
 
-func removeConversionWebhook(obj *unstructured.Unstructured) {
-	crd := &apiextensionsv1.CustomResourceDefinition{}
-	if err := scheme.Convert(obj, crd, nil); err != nil {
-		panic(err)
-	}
-	crd.Spec.Conversion = nil
-	if err := scheme.Convert(crd, obj, nil); err != nil {
-		panic(err)
-	}
-}
-
 // isCRDGroup checks whether the object provided is a CRD for the specified API group.
 func isCRDGroup(obj *unstructured.Unstructured, group string) bool {
 	switch obj.GetKind() {
@@ -341,7 +301,7 @@ func mergeMaps[K comparable, V any](maps ...map[K]V) map[K]V {
 func addInfraClusterProtectionPolicy(objs []unstructured.Unstructured, providerName string) []unstructured.Unstructured {
 	policy := &unstructured.Unstructured{
 		Object: map[string]interface{}{
-			"apiVersion": "admissionregistration.k8s.io/v1beta1",
+			"apiVersion": "admissionregistration.k8s.io/v1",
 			"kind":       "ValidatingAdmissionPolicy",
 			"metadata": map[string]interface{}{
 				"name": "openshift-cluster-api-protect-" + providerName + "cluster",
@@ -374,7 +334,7 @@ func addInfraClusterProtectionPolicy(objs []unstructured.Unstructured, providerN
 
 	binding := &unstructured.Unstructured{
 		Object: map[string]interface{}{
-			"apiVersion": "admissionregistration.k8s.io/v1beta1",
+			"apiVersion": "admissionregistration.k8s.io/v1",
 			"kind":       "ValidatingAdmissionPolicyBinding",
 			"metadata": map[string]interface{}{
 				"name": "openshift-cluster-api-protect-" + providerName + "cluster",
